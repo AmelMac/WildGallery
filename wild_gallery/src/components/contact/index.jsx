3985a9941efa356c76e7d8c0d26d40ead35b4bb9
Amelie Macquet <amelie.macquet.am@gmail.com>
2021-02-09 15:21:03 +0100

    install react hook form and did some style

--- wild_gallery/src/components/contact/index.jsx
+++ wild_gallery/src/components/contact/index.jsx
@@ -1,10 +1,41 @@
 import React from "react";
-import { H1 } from "./style";
+import { H1, Container } from "./style";
+import { useForm } from "react-hook-form";
 
 export default function Contact() {
-    return (
-      <div>
-        <H1>CONTACT US</H1>
-      </div>
-    );
-  }
+  const { register, handleSubmit, errors } = useForm();
+  const onSubmit = (data) => console.log(data);
+  console.log(errors);
+  return (
+    <div>
+      <H1>CONTACT US</H1>
+      <Container>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <input
+            type="text"
+            placeholder="Last name"
+            name="Last name"
+            ref={register({ required: true, maxLength: 100 })}
+          />
+          <input
+            type="text"
+            placeholder="First name"
+            name="First name"
+            ref={register({ required: true, maxLength: 80 })}
+          />
+          <input
+            type="text"
+            placeholder="Email"
+            name="Email"
+            ref={register({ required: true, pattern: /^\S+@\S+$/i })}
+          />
+          <textarea
+            name="Message"
+            ref={register({ required: true, max: 500, min: 10 })}
+          />
+          <input className="test" type="submit" value="ENVOYER" />
+        </form>
+      </Container>
+    </div>
+  );
+}
--- wild_gallery/src/components/contact/style.js
+++ wild_gallery/src/components/contact/style.js
@@ -1,7 +1,29 @@
 import styled from "styled-components";
 
 export const H1 = styled.h1`
-display: flex;
-font-size: 12rem;
-padding: 16vh;
-`
+  display: flex;
+  font-size: 12rem;
+  padding: 16vh;
+`;
+export const Container = styled.div`
+  form {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-bottom: 15vh;
+  }
+  input {
+      margin: 1vw;
+      height: 4vh;
+      width: 40%;
+  }
+  textarea {
+      margin: 1vw;
+      height: 15vh;
+      width: 40%;
+  }
+  .test {
+      width: 15%;
+      height: 5vh;
+  }
+`;
